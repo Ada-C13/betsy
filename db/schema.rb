8af4a2f9dbb03a4ba1264ea05fe80c4020cdb82a
Catherina <catherine.pchelina@gmail.com>
2020-06-09 16:18:55 -0700

    Add relation between reviews and products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_231104) do
+ActiveRecord::Schema.define(version: 2020_06_09_231652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_231104) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
 end
