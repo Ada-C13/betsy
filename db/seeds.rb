dac0d94958d7da17acfbdfff3953c2e89c665fd9
Nikki Vaughan <vnikki13@gmail.com>
2020-06-10 14:50:39 -0700

    created order seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'csv'
 
+# Merchant seeds
 MERCHANT_FILE = Rails.root.join('db', 'seed_data', 'merchants.csv')
 puts "Loading raw product data from #{MERCHANT_FILE}"
 
@@ -21,6 +22,7 @@ end
 puts "Added #{Merchant.count} merchant records"
 puts "#{merchant_failures.length} merchants failed to save"
 
+# Product seeds
 PRODUCT_FILE = Rails.root.join('db', 'seed_data', 'products.csv')
 puts "Loading raw product data from #{PRODUCT_FILE}"
 
@@ -44,6 +46,34 @@ end
 
 puts "Added #{Product.count} product records"
 puts "#{product_failures.length} products failed to save"
+
+# Order seeds
+ORDER_FILE = Rails.root.join('db', 'seed_data', 'orders.csv')
+puts "Loading raw order data from #{ORDER_FILE}"
+
+order_failures = []
+CSV.foreach(ORDER_FILE, :headers => true) do |row|
+  order = Order.new
+  order.id = row['id']
+  order.status = row['status']
+  order.credit_card_num = row['credit_card_num']
+  order.credit_card_exp = row['credit_card_exp']
+  order.credit_card_cvv = row['credit_card_cvv']
+  order.address = row['address']
+  order.city = row['city']
+  order.state = row['state']
+  order.zip = row['zip']
+  order.time_submitted = row['time_submitted']
+  order.customer_email = row['customer_email']
+
+  successful = order.save
+  if !successful
+    order_failures << order
+  end
+end
+
+puts "Added #{Order.count} order records"
+puts "#{order_failures.length} orders failed to save"
 
 # reloading postgres for the latest ID
 puts "Manually resetting PK sequence on each table"
