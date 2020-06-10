f2f5146d0d0c6a01564fe3d90032d0bfac7b0a91
Nikki Vaughan <vnikki13@gmail.com>
2020-06-09 17:01:26 -0700

    model is set up with test skeletons

---
+++ test/models/order_item_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+describe OrderItem do
+  describe 'relationships' do
+    it 'must belong to a Product' do
+
+    end
+
+    it 'must belong to an Order' do
+
+    end
+  end
+
+  descirbe 'validations' do
+    it 'is valid if quantity is greater than 0' do
+
+    end
+
+    it 'is not valid if quantity is less than or equal to 0' do
+
+    end
+  end
+end
