bee868d1ee5a9400c228a967db95b1a7fe33b778
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-10 14:27:11 -0700

    adds show and edit actions + tests to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+  before_action only: [:show, :edit, :update, :destroy] do
+    find_order(params[:id])
+  end
+  
+  def show
+  end
+  
+  def create
+    # when a customer adds a product to their cart
+    # no products in cart: a new order is created, order_id stored in session?, an order_item is created and put into the cart
+    # products in cart: an order_item is created and put into the cart
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_order(id)
+    @order = Order.find_by(id: id)
+    head :not_found if !@order
+    return @order
+  end
 end
--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 describe OrdersController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @order = Order.create!
+  end
+  
+  describe "show" do
+    it "responds with success when showing an existing valid order" do
+      order = Order.create!
+      get order_path(order.id)
+      must_respond_with :success
+    end
+
+    it "responds with 404 with an invalid order id" do
+      get order_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "create" do
+
+  end
+
+  describe "edit" do
+    it "responds with success when getting the edit page for an existing valid order" do
+      order = Order.create!
+      get edit_order_path(order.id)
+      must_respond_with :success
+    end
+
+    it "responds with 404 when getting the edit page with an invalid order id" do
+      get edit_order_path(-1)
+      must_respond_with :not_found
+    end
+  end
+
+  describe "update" do
+
+  end
+
+  describe "destroy" do
+
+  end
 end
