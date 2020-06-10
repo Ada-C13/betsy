ec60a894405d8dcba9d02a6ada68dd6b854b948b
Haben <haben7@gmail.com>
2020-06-10 00:47:51 -0700

    added migration to relate reviews to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_070047) do
+ActiveRecord::Schema.define(version: 2020_06_10_074429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,10 @@ ActiveRecord::Schema.define(version: 2020_06_10_070047) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   add_foreign_key "products", "merchants"
+  add_foreign_key "reviews", "products"
 end
