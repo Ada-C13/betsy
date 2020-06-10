fdc657731585824e4be9b09b588557decd6db761
Angela Nguyen <angethuy@gmail.com>
2020-06-10 00:30:07 -0700

    added model testing for category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
+
+  validates :name, presence: true, uniqueness: {
+    message: "Category: %{value} already exists." 
+  }
+
 end
