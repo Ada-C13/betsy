8a50fcc82f73406f4d7dddadd3b1fc40b31fc3df
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-10 10:43:35 -0700

    adds routes for all controller actions, orders routes may need updating later

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # products
+  resources :products
+
+  # categories
+  resources :categories, only: [:show, :new, :create]
+
+  # reviews
+  resources :reviews, only: [:new, :create]
+  
+  # merchants
+  get "/merchant/:id", to: "merchants#show", as: "merchant"
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create"
+  delete "/logout", to: "merchants#destroy", as: "logout"
+
+  # orders
+  resources :orders, except: :destroy # index action would need to be nested in myaccount/orders
+
+  # order_items
+  resources :order_items, except: [:index, :show] # an order item is created when the user adds a product to cart
 end
