e0fb8c1c3ff14d9c551e0bf50765b04d7178d2ef
Olga Patrakova <olgapatrakova@outlook.com>
2020-06-10 15:06:40 -0700

    Controller is able to show all products or products of a specific merchant in products index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,13 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:merchant_id]
+      # This is the nested route, /merchants/:merchant_id/products
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      @products = merchant.products
+    else
+      # This is the 'regular' route, /products
+      @products = Product.all
+    end
   end
 
   def show
