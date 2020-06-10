b9b3be95eecfbabc7dd33e70ee730a158e6b0c2e
SuelyBarreto <suely@barreto.us>
2020-06-09 19:42:55 -0700

    Added validations to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_items, dependent: :destroy
+
+  VALID_STATUS = %w(pending paid complete cancelled)
+  validates :status, presence: true, inclusion: {in: VALID_STATUS}
 end
