9ceb3819752b2b77cd5054c69adefd2c8ac788f4
Haben <haben7@gmail.com>
2020-06-10 02:01:59 -0700

    added review model tests, validatio test not passing

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
 
   belongs_to :product
 
-  validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
-
+  validates :rating, :numericality => { :in => 0..5, :message => " should be between 0 to 5" }
+  #validates  :rating, :numericality: { greater_than: 0, less_than_or_equal_to: 5 }
 end
