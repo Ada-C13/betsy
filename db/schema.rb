982453d654aa8f0345df36a2c5e30afceb32cf9b
Nikki Vaughan <vnikki13@gmail.com>
2020-06-09 17:41:20 -0700

    created customer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_234951) do
+ActiveRecord::Schema.define(version: 2020_06_10_004051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
