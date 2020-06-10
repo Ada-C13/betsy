c1253ad552e126c4d20ecf3ccb7d842fb59d2dd8
Catherina <catherine.pchelina@gmail.com>
2020-06-10 12:10:16 -0700

    Add create action and product params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,25 @@ class ProductsController < ApplicationController
   end
 
   def create 
+    @product = Product.new(product_params)
+
+    if @product.save 
+      flash[:success] = "Successfully created #{@product.title}"
+      redirect_to product_path(@product.id)
+
+      return
+    else
+      render :new 
+      return
+    end
   end
+
+
+  private 
+
+  def product_params 
+    return params.require(:product).permit(:title, :price, :description,
+                                           :photo_url, :stock)
+  end
+
 end
