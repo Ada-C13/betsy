c5612641dc2a2ba65cff61fc0e5a4296e4dae6ea
SuelyBarreto <suely@barreto.us>
2020-06-10 20:54:19 -0700

    Added routes for orders, products, merchants, reviews and categories as documented on Google doc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,25 @@
 Rails.application.routes.draw do
   root 'homepages#index'
 
-resources :merchants # to be changed later
+  resources :orders, except: [:new, :create]
+  post "/orders/:id/pay", to: "orders#pay", as: "order_pay"
+  post "/orders/:id/complete", to: "orders#complete", as: "order_complete"
+  post "/orders/:id/cancel", to: "orders#cancel", as: "order_cancel"
 
- # Omniauth Login route
- get "/auth/github", as: "github_login"
+  resources :products
+  post "/products/:id/deactivate", to: "products#deactivate", as: "product_deactivate"
 
- # Omniauth Github callback route
- get "/auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
- 
+  resources :merchants, only: [:show, :create]
+  post "/merchants/:id/deactivate", to: "merchants#deactivate", as: "merchant_deactivate"
+  get "/merchants/:id/dashboard", to: "merchants#dashboard", as: "merchant_dashboard"
+  
+  # Omniauth Login route
+  get "/auth/github", as: "github_login"
+  # Omniauth Github callback route
+  get "/auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
+  
+  resources :reviews, only: [:new, :create]
+
+  resources :categories, except: [:edit, :update, :destroy]
 
 end
