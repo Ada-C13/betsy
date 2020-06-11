2762bbafe6c80fbaeb95713d6c3dab2b8c5e31d1
Yaz <yasmineoshaugh@gmail.com>
2020-06-10 23:16:53 -0700

    added email regexp validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,8 @@ class Order < ApplicationRecord
 
   with_options if: :order_submitted? do |submitted|
     submitted.validates :name, presence: true
-    submitted.validates :email, presence: true
+    submitted.validates :email, presence: true, 
+    format: { with: /[a-z0-9\+\-_\.]+@[a-z\d\-]+\.[a-z]+\z/, message: "email is not valid" }
     submitted.validates :address, presence: true
     submitted.validates :cc_last_four, presence: true, 
       format: { with: /\d{4}/, message: "cc_last_four is not valid" }
