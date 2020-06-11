9f3ebaa17a8c29d7e021e45fc679e8449998099b
Haben <haben7@gmail.com>
2020-06-10 18:37:28 -0700

    minor edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
+
+resources :merchants # to be changed later
+
  # Omniauth Login route
  get "/auth/github", as: "github_login"
 
  # Omniauth Github callback route
  get "/auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
+ 
 
 end
