ac284864b5e2be489dfce40241957f73dfaf3440
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-11 00:29:11 -0700

    defines order_items#create action, products can be added to orders

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,18 +4,35 @@ class OrderItemsController < ApplicationController
   end
 
   def create
-    # if session[:order_id] is nil 
-      # create blank order
-      # set session[:order_id] order.id
+    # creates a new order if there is no order_id saved to session
+    if !session[:order_id]
+      order = Order.create!
+      session[:order_id] = order.id
+    end
 
-    # find product by params[:product_id] (nested route) -> products/:product_id/add, params: quantity
-      # not_found if there is no product with that id
+    # find product by the id passed in thru form
+    product = Product.find_by(id: params[:product_id])
+    return head :not_found if !product
 
-    # create new order_item with product_id, order_id, quantity from params, and shipped default to false 
-      # if quantity is greater than product stock, flash error message, return to product show page
-      # else flash success message
-    
-      # redirect to cart
+    # create new order_item
+    order_item = OrderItem.new(
+      product_id: product.id,
+      order_id: order.id,
+      quantity: params[:product][:quantity], # TODO: Confirm whether product is the correct key name
+      shipped: false
+    )
+
+    # if quantity is greater than product stock, don't save order_item
+    if order_item.quantity > product.stock
+      flash[:error] = "A problem occurred: #{product.title} does not have enough quantity in stock"
+      redirect_to product_path(product.id)
+      return
+    else 
+      order_item.save
+      flash[:success] = "Successfully added #{product.title} to cart!"
+      redirect_to product_path(product.id)
+      return
+    end
   end
 
   def update
