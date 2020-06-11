77bf18cbbf7330c5921a84f212d502751b46d63f
Haben <haben7@gmail.com>
2020-06-10 18:58:45 -0700

    added uid and provider table to merchants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_204945) do
+ActiveRecord::Schema.define(version: 2020_06_11_001518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2020_06_10_204945) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
+    t.string "provider"
   end
 
   create_table "order_items", force: :cascade do |t|
