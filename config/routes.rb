70f600e04e5e8e272470dc85498c22991e41adaa
Rosalyn <syrosalynyu@gmail.com>
2020-06-11 16:42:48 -0700

    initiated omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'categories/new'
   get 'merchants/index'
   get 'merchants/show'
+
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "merchants#create" as: "omniauth_callback"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
