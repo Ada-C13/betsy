e9f434f3ce98448c1145158ba8fd7ad8df493c36
Yaz <yasmineoshaugh@gmail.com>
2020-06-10 23:05:21 -0700

    changed cc_last_four validation: string validation to match the schema, used regexp to check for digits and length

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,10 @@ class Order < ApplicationRecord
     submitted.validates :name, presence: true
     submitted.validates :email, presence: true
     submitted.validates :address, presence: true
-    submitted.validates :cc_last_four, presence: true#, numericality: { only_integer: true }
+    submitted.validates :cc_last_four, presence: true, 
+      format: { with: /\d{4}/, message: "cc_last_four is not valid" }
     submitted.validates :cc_exp, presence: true
-    submitted.validates :cc_cvv, presence: true#, numericality: { only_integer: true }
+    submitted.validates :cc_cvv, presence: true, numericality: { only_integer: true }
   end
   validates :status, inclusion: { in: %w(pending paid shipped), message: "%{value} is not a valid status" }
 
