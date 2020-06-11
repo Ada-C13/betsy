09666c89d08641eb1b368ca73ca200714210edef
Yaz <yasmineoshaugh@gmail.com>
2020-06-10 22:40:45 -0700

    added conditional validations to all fields needed for order submit, tested in console, passing

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,12 +2,16 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  # validates :name, presence: true
-  # validates :email, presence: true
-  # validates :address, presence: true
-  # validates :cc_last_four, presence: true, numericality: { only_integer: true }
-  # validates :cc_exp, presence: true
-  # validates :cc_cvv, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true, if: :order_submitted?
+  validates :email, presence: true, if: :order_submitted?
+  validates :address, presence: true, if: :order_submitted?
+  validates :cc_last_four, presence: true, if: :order_submitted?, numericality: { only_integer: true }
+  validates :cc_exp, presence: true, if: :order_submitted?
+  validates :cc_cvv, presence: true, if: :order_submitted?, numericality: { only_integer: true }
   validates :status, inclusion: { in: %w(pending paid shipped), message: "%{value} is not a valid status" }
+
+  def order_submitted?
+    status == "paid" || status == "shipped"
+  end
 
 end
