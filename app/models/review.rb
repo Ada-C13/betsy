db98db53ed515fc035b1d5c28b309cb358c1b95c
Yaz <yasmineoshaugh@gmail.com>
2020-06-11 13:52:29 -0700

    removed invisible terminating character

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
   
-  validates :rating, presence: true #, numericality: { only_integer: true }, inclusion: { in: %w(1 2 3 4 5), message: "%{value} is not a valid rating" } 
+  validates :rating, presence: true #, numericality: { only_integer: true }, inclusion: { in: %w(1 2 3 4 5), message: "%{value} is not a valid rating" }
   validates :feedback, length: { minimum: 20, maximum: 500 }
 end
