16ef980f4209a8d44f474be53b51fd1d010f01f9
Angela Nguyen <angethuy@gmail.com>
2020-06-11 13:22:54 -0700

    renamed homepages to pages

--- app/controllers/homepages_controller.rb
+++
@@ -1,4 +0,0 @@
-class HomepagesController < ApplicationController
-  def landing
-  end
-end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-describe HomepagesController do
+describe PagesController do
   # it "does a thing" do
   #   value(1+1).must_equal 2
   # end
