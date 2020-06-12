22d65dfc8bf27edd34a1efd7fb03f0619aa3b1da
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-11 23:57:23 -0700

    add routes and actions in cotroller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,28 @@
 class CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created #{@category.id}"
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create a category"
+      flash[:messages] = @work.errors.messages
+      render :new, status: :bad_request
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
