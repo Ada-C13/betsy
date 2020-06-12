9e2a796efaef69ad2c41ca90546b49f98231aafe
Haben <haben7@gmail.com>
2020-06-11 18:24:53 -0700

    changed create to login in merchants_controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -10,9 +10,8 @@ class MerchantsController < ApplicationController
   end
 
 
-  def create
+  def login #create
     auth_hash = request.env["omniauth.auth"]
-
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
     if merchant
       # User was found in the database
@@ -21,9 +20,10 @@ class MerchantsController < ApplicationController
       # User doesn't match anything in the DB
       # Attempt to create a new user
       merchant = Merchant.build_from_github(auth_hash)
-
+     
       if merchant.save
         flash[:success] = "Logged in as new merchant #{merchant.username}"
+    
       else
        
         flash[:error] = "Could not create new user account: #{merchant.errors.messages}"
@@ -37,6 +37,7 @@ class MerchantsController < ApplicationController
   end
 
   def current
+  
     unless @current_merchant
       flash[:error] = "You must be logged in to see this page"
       redirect_to root_path
@@ -44,7 +45,7 @@ class MerchantsController < ApplicationController
     end
   end
 
-  def destroy
+  def logout
     session[:merchant_id] = nil
     flash[:success] = "Successfully logged out!"
 
