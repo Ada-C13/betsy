85a04943ac61a84ba63ee630356f1213fa441d41
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-12 15:05:27 -0700

    commit because i have to marge

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 describe CategoriesController do
   before do
-    @categories = Category.new(name: "pets")
+    @categories = Category.new(name: "category")
   end
-  let (:category_hash) {
-    {
-      ategory: {
-        name: "pets", 
-      }
-    }
-  }
+  # let (:category_hash) {
+  #   {
+  #     category: {
+  #       name: "pets", 
+  #     }
+  #   }
+  # }
 
   describe "index" do
     it "responds with success when there are many categories saved" do
@@ -34,18 +34,31 @@ describe CategoriesController do
     end
   end
 
+  describe "create" do
+    before do
+      @category_data = {
+        category: {
+          name: 'test category'
+        }
+      }
+    end
+    it "can create a new category with valid information accurately, and redirect" do
+      # Ensure that there is a change of 1 in Category.count
+      expect { 
+        post categories_path, params: @category_data
+      }.must_differ "Category.count", 1
+
+       
+      # Find the newly created Category, and check that all its attributes match what was given in the form data
+      expect(Category.last.name).must_equal @category_data[:category][:name]
+   
+      # Check that the controller redirected the user
+      must_redirect_to categories_path(Category.last.id)
+    end
+  end
 
 
 
 
-  # it "must get index" do
-  #   get categories_index_url
-  #   must_respond_with :success
-  # end
-
-  # it "must get new" do
-  #   get categories_new_url
-  #   must_respond_with :success
-  # end
 
 end
