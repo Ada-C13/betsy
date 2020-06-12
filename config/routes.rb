2fa70ff82e1f87a7fb85779b13f658360bdad303
Rosalyn <syrosalynyu@gmail.com>
2020-06-12 15:04:05 -0700

    removed duplicated routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,17 +20,13 @@ Rails.application.routes.draw do
   get "merchants/:id", to: "merchants#account", as: "account"
   # TODO: (Ross) In order to make this nested route works, we have to wait for Lak to merge her Product controller with the "index" action 
   resources :merchants do
+    # this nested route will trigger the "index" action in products controller from Lak
     resources :products, only: [:index]
-    # TODO: add the nested route merchants/:id/orders to show all the orders
+    # this nested route will trigger the "index" action in orders controller from Yaz
+    resources :orders, only: [:index]
   end
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create", as: "omniauth_callback"
   put "/logout", to: "merchants#logout", as: "logout"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # TODO: (Ross) I think we need a new controller for our homepage, and that will be the root path
-  # as below, for now, I will set the root == products
-  root to: 'products#index'
-  get "/products", to: "products#index", as: "products"
-  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html  
 end
