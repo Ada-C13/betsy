00912350cbe5f81a992a6726b154ffaa80fd2b7f
Yaz <yasmineoshaugh@gmail.com>
2020-06-11 23:33:26 -0700

    restructured validations to remove bug, added shells for methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,21 +2,31 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  with_options if: :order_submitted? do |submitted|
-    submitted.validates :name, presence: true
-    submitted.validates :email, presence: true, 
-    format: { with: /[a-z0-9\+\-_\.]+@[a-z\d\-]+\.[a-z]+\z/, message: "email is not valid" }
-    submitted.validates :address, presence: true
-    submitted.validates :cc_last_four, presence: true 
-      # format: { with: /\d{4}/, message: "cc_last_four is not valid" }
-    submitted.validates :cc_exp, presence: true
-    submitted.validates :cc_cvv, presence: true, numericality: { only_integer: true }
-  end
-  validates :status, inclusion: { in: %w(pending paid shipped)}
-  # , message: "#{value} is not a valid status" 
-
+  validates :status, presence: true, inclusion: { in: %w(pending paid shipped), message: "Status must be pending, paid, or shipped"} 
   def order_submitted?
     status == "paid" || status == "shipped"
   end
+  with_options if: :order_submitted? do |submitted|
+    submitted.validates :name, presence: true
+    submitted.validates :email, presence: true, 
+      format: { with: /[a-z0-9\+\-_\.]+@[a-z\d\-]+\.[a-z]+\z/, message: "email is not valid" }
+    submitted.validates :address, presence: true
+    submitted.validates :cc_last_four, presence: true, 
+      format: { with: /\d{4}/, message: "cc_last_four is not valid" }
+    submitted.validates :cc_exp, presence: true
+    submitted.validates :cc_cvv, presence: true, numericality: { only_integer: true }
+  end
+
+  def submit_order
+
+  end
+  def clear_cart
+  end 
+  def total_price
+  end 
+  def update_status
+  end
+  
+
 
 end
