f1ef1fa36764b8d1326416b707d4e9678ea589bc
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-12 13:13:47 -0700

    add routes for categories action indax, new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  # get 'categories/index'
-  # get 'categories/new'
   get 'merchants/index'
   get 'merchants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:index, :show, :create]
-  # get 'merchants/index'
-  # get 'merchants/show'
+
+  resources :categories, only: [:index, :new, :create]
+
 
 
 
