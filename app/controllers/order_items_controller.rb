737d7e41230fa903df17f08e565491f24bd70453
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-11 20:13:00 -0700

    finalizes moving order_items#create action to products#add_to_cart, updates tests

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,38 +3,6 @@ class OrderItemsController < ApplicationController
     find_order_item(params[:id])
   end
 
-  # def create
-  #   # creates a new order if there is no order_id saved to session
-  #   if !session[:order_id]
-  #     order = Order.create(status: "pending")
-  #     session[:order_id] = order.id
-  #   end
-
-  #   # find product by the id passed in thru form
-  #   product = Product.find_by(id: params[:product_id])
-  #   return head :not_found if !product
-
-  #   # create new order_item
-  #   order_item = OrderItem.new(
-  #     product_id: product.id,
-  #     order_id: session[:order_id],
-  #     quantity: params[:product][:quantity], # TODO: Confirm whether product is the correct key name (if the form uses the product model, this should be right)
-  #     shipped: false
-  #   )
-
-  #   # if quantity is greater than product stock, don't save order_item
-  #   if order_item.quantity > product.stock
-  #     flash[:error] = "A problem occurred: #{product.title} does not have enough quantity in stock"
-  #     redirect_to product_path(product.id)
-  #     return
-  #   else 
-  #     order_item.save
-  #     flash[:success] = "Successfully added #{product.title} to cart!"
-  #     redirect_to product_path(product.id)
-  #     return
-  #   end
-  # end
-
   def update
     quantity = params[:order_item][:quantity].to_i # TODO: Confirm whether order_item is the correct key name (if the form uses the order_item model, this should be right)
     product = @order_item.product
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,24 +1,4 @@
 class ProductsController < ApplicationController
-  def index
-    if params[:merchant_id]
-      # This is the nested route, /merchants/:merchant_id/products
-      merchant = Merchant.find_by(id: params[:merchant_id])
-      @products = merchant.products
-    else
-      # This is the 'regular' route, /products
-      @products = Product.all
-    end
-  end
-
-  def show
-    @product = Product.find_by(id: params[:id])
-
-    if @product.nil?
-      redirect_to products_path
-      return
-    end
-  end
-
   def new 
     @product = Product.new
   end
@@ -64,15 +44,15 @@ class ProductsController < ApplicationController
   end
 
   def add_to_cart
-    # creates a new order if there is no order_id saved to session
-    if !session[:order_id]
-      order = Order.create(status: "pending")
-      session[:order_id] = order.id
-    end
-
     # find product by the id passed in thru params
     product = Product.find_by(id: params[:id])
     return head :not_found if !product
+
+    # creates a new order if there is no order_id saved to session
+    if !session[:order_id]
+      order = Order.create
+      session[:order_id] = order.id
+    end
 
     # create new order_item
     order_item = OrderItem.new(
@@ -81,7 +61,7 @@ class ProductsController < ApplicationController
       quantity: params[:quantity],
       shipped: false
     )
-
+    p order_item.quantity
     # if quantity is greater than product stock, don't save order_item
     if order_item.quantity > product.stock
       flash[:error] = "A problem occurred: #{product.title} does not have enough quantity in stock"
@@ -91,6 +71,26 @@ class ProductsController < ApplicationController
       order_item.save
       flash[:success] = "Successfully added #{product.title} to cart!"
       redirect_to product_path(product.id)
+      return
+    end
+  end
+
+  def index
+    if params[:merchant_id]
+      # This is the nested route, /merchants/:merchant_id/products
+      merchant = Merchant.find_by(id: params[:merchant_id])
+      @products = merchant.products
+    else
+      # This is the 'regular' route, /products
+      @products = Product.all
+    end
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      redirect_to products_path
       return
     end
   end
