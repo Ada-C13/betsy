320ace2a6aeee0b92e61baef5d352aa331602714
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-12 00:02:39 -0700

    Implemented CRUD for product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,77 @@
 class ProductsController < ApplicationController
+  
+  before_action :find_product, only: [:show, :edit, :update, :destroy] # making my codes DRY
+  
+  def index
+    @products = Product.all
+  end
+
+  def show
+    if @product.nil?
+      redirect_to proucts_path
+      return
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:success] = " Successfully created #{@product.name}"
+      redirect_to products_path
+      return
+    else
+      flash.now[:warning] = "A problem occurred: Could not create product"
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  def edit
+    if @product.nil?
+      redirect_to products_path
+      return
+    end
+  end
+
+  def update
+    if @product.nil?
+      head :not_found
+      return
+    elsif @product.update(product_params)
+      flash[:success] = " Successfully updated #{@product.name}"
+      redirect_to product_path(@product)
+      return
+    else 
+      flash.now[:error] = "A problem occurred: Could not update #{@product.name} "
+      render :edit, status: :bad_request
+      return
+    end
+  end
+
+  def destroy
+    if @product.nil?
+      head :not_found
+      return
+    else
+      @product.destroy
+      flash[:success] = "Successfully deleted #{@product.name}"
+      redirect_to products_path
+      return
+    end
+  end 
+  
+  private
+
+  def product_params
+    return params.require(:product).permit(:name, :price, :stock, :active, :description, :photo)
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'homepages/index'
   get 'categories/index'
   get 'categories/new'
   get 'merchants/index'
   get 'merchants/show'
+
+
+  root to: "homepages#index"
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
