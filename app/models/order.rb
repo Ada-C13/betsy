ad53ae8d6c44fa8363adcaf018133edee8b78a5d
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-12 02:14:38 -0700

    fleshes out orders#show view, adds two model methods for order_item subtotal and order total

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
+
+  def total
+    return order_items.map{ |item| item.subtotal}.sum
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def subtotal
+    return product.price * quantity
+  end
 end
