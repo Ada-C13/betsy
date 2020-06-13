1315e8552b6fc4620080f602ad917b56045825bb
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-12 22:11:48 -0700

    defines orders#complete action, moves session reset logic into orders#show action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action only: [:index, :show, :checkout, :update, :destroy] do
+  before_action only: [:index, :checkout, :complete, :destroy] do
     find_order(session[:order_id])
   end
   
@@ -10,11 +10,13 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find_by(id: params[:id])
 
-    if !@order || @order.id != session[:order_id] 
+    if !@order || @order.id != session[:order_id] # we need session[:order_id] in order to view the show page
       flash[:status] = :failure
-      flash[:result_text] = "A problem occurred: You cannot view this order!"
+      flash[:result_text] = "You cannot view this order!"
       redirect_to root_path
       return
+    elsif @order.status == "paid" # if the order has been completed, we will reset the session[:order_id]
+      session[:order_id] = nil
     end
   end
 
@@ -23,7 +25,7 @@ class OrdersController < ApplicationController
       @order.update(status: "pending")
     elsif !@order || @order_items.empty?
       flash[:status] = :failure
-      flash[:result_text] = "A problem occurred: You cannot check out without any products in your cart!"
+      flash[:result_text] = "You cannot check out without any products in your cart!"
       redirect_to cart_path
       return
     end
@@ -31,10 +33,32 @@ class OrdersController < ApplicationController
 
   def complete
     # saves user info to order
-    # reduces the stock of each product
-    # changes status to paid
-    # session[:order_id] = nil
-    # redirects to checkout finalize page, which needs a route
+    if @order.update(order_params)
+
+      # stores the last 4 digits of cc_number
+      @order.update(cc_number: @order.cc_number.to_s[-4..-1].to_i) 
+
+      # reduces the stock of each product 
+      # QUESTION: Should this be moved into a product model method?
+      @order.order_items.each do |item|
+        item.product.update(stock: item.product.stock - item.quantity)
+      end
+      
+      # change status to paid
+      @order.update(status: "paid")
+
+      # display flash messages and redirect
+      flash[:status] = :success
+      flash[:result_text] = "Thank you for your order, #{@order.name}!"
+      redirect_to order_path(@order)
+      return
+    else
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Could not complete order!"
+      flash.now[:messages] = @order.errors.messages
+      render :checkout, status: :bad_request
+      return
+    end
   end
 
   def destroy
@@ -49,6 +73,6 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    return params.require(:order).permit(:name, :email, :mailing_address, :cc_number, :cc_exp) # TODO: Do we want to store CVV and billing zip even if we're not showing it?
+    return params.require(:order).permit(:name, :email, :mailing_address, :cc_number, :cc_exp)
   end
 end
