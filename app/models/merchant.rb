83ed5d9a9b5192c5d0fda502dd53a899dda41810
Catherina <catherine.pchelina@gmail.com>
2020-06-12 22:01:49 -0700

    Add total_revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,23 @@ class Merchant < ApplicationRecord
     #user has not been saved, will be saved outside of this method
     return merchant
   end
+
+  def total_revenue
+    merchant_products = self.products 
+    merchant_orders = []
+    merchant_products.each do |product|
+      merchant_orders += product.orders
+    end
+    merchant_orders.uniq! 
+    valid_orders = merchant_orders.select { |order| order.status != "cancelled" }
+
+    total_revenue = 0.0
+    valid_orders.each do |order|
+      order.order_items.each do |item|
+        total_revenue += item.quantity * item.product.price
+      end
+    end
+
+    return total_revenue
+  end
 end
