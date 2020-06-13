61ac59366c54f349b03e399a6af794c6c148b8a8
Catherina <catherine.pchelina@gmail.com>
2020-06-13 12:46:06 -0700

    Fix total revenue method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,44 +12,65 @@ class Merchant < ApplicationRecord
     return merchant
   end
 
-  def total_revenue
-    valid_orders = get_valid_orders
 
-    total_revenue = 0.0
-    valid_orders.each do |order|
-      order.order_items.each do |item|
-        total_revenue += item.quantity * item.product.price
-      end
-    end
+  # def total_revenue
+  #   valid_orders = get_valid_orders
 
-    return total_revenue
-  end
+  #   total_revenue = 0.0
+  #   valid_orders.each do |order|
+  #     order.order_items.each do |item|
+  #       total_revenue += item.quantity * item.product.price
+  #     end
+  #   end
 
-  def revenue_for(status)
-    valid_orders = get_valid_orders
+  #   return total_revenue
+  # end
 
-    total_revenue = 0.0
-    specific_status_orders = valid_orders.select { |order| order.status == status }
-    specific_status_orders.each do |order|
-      order.order_items.each do |item|
-        total_revenue += item.quantity * item.product.price
-      end
-    end
+  # def revenue_for(status)
+  #   valid_orders = get_valid_orders
 
-    return total_revenue
-  end
+  #   total_revenue = 0.0
+  #   specific_status_orders = valid_orders.select { |order| order.status == status }
+  #   specific_status_orders.each do |order|
+  #     order.order_items.each do |item|
+  #       if item.product.merchant_id == self.id
+  #         total_revenue += item.quantity * item.product.price
+  #       end
+  #     end
+  #   end
+
+  #   return total_revenue
+  # end
 
 
-  private 
+  # private 
 
-  def get_valid_orders 
-    merchant_products = self.products 
-    merchant_orders = []
-    merchant_products.each do |product|
-      merchant_orders += product.orders
-    end
-    merchant_orders.uniq! 
+  # def get_valid_orders 
+  #   merchant_products = self.products 
+  #   merchant_orders = []
+  #   merchant_products.each do |product|
+  #     merchant_orders += product.orders
+  #   end
+  #   merchant_orders.uniq! 
     
-    return merchant_orders.select { |order| order.status != "cancelled" }
+  #   return merchant_orders.select { |order| order.status != "cancelled" }
+  # end
+
+  def total_revenue
+    merchant_products = self.products 
+    total_revenue = 0.0
+
+    puts "MERCH PRODUCTS = #{merchant_products.first.order_items.first}"
+
+    merchant_products.each do |product|
+      puts "PRODUCT = #{product.order_items.count}"
+      product.order_items.each do |item|
+        
+        puts "PRODUCT ORDER ITEM = #{item}"
+        total_revenue += item.quantity * item.product.price
+      end 
+    end
+
+    return total_revenue
   end
 end
