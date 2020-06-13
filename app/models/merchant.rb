07313eb13fa739956c00cfa09135cc45413e86ea
Catherina <catherine.pchelina@gmail.com>
2020-06-13 13:45:05 -0700

    Add number of orders by status method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -13,63 +13,16 @@ class Merchant < ApplicationRecord
   end
 
 
-  # def total_revenue
-  #   valid_orders = get_valid_orders
-
-  #   total_revenue = 0.0
-  #   valid_orders.each do |order|
-  #     order.order_items.each do |item|
-  #       total_revenue += item.quantity * item.product.price
-  #     end
-  #   end
-
-  #   return total_revenue
-  # end
-
-  # def revenue_for(status)
-  #   valid_orders = get_valid_orders
-
-  #   total_revenue = 0.0
-  #   specific_status_orders = valid_orders.select { |order| order.status == status }
-  #   specific_status_orders.each do |order|
-  #     order.order_items.each do |item|
-  #       if item.product.merchant_id == self.id
-  #         total_revenue += item.quantity * item.product.price
-  #       end
-  #     end
-  #   end
-
-  #   return total_revenue
-  # end
-
-
-  # private 
-
-  # def get_valid_orders 
-  #   merchant_products = self.products 
-  #   merchant_orders = []
-  #   merchant_products.each do |product|
-  #     merchant_orders += product.orders
-  #   end
-  #   merchant_orders.uniq! 
-    
-  #   return merchant_orders.select { |order| order.status != "cancelled" }
-  # end
-
   def total_revenue
     merchant_products = self.products 
     total_revenue = 0.0
 
-    puts "MERCH PRODUCTS = #{merchant_products.first.order_items.first}"
-
     merchant_products.each do |product|
-      puts "PRODUCT = #{product.order_items.count}"
       product.order_items.each do |item|
         if item.order.status == "cancelled"
           next
         end
         
-        puts "PRODUCT ORDER ITEM = #{item}"
         total_revenue += item.quantity * item.product.price
       end 
     end
@@ -90,5 +43,20 @@ class Merchant < ApplicationRecord
     end
 
     return total_revenue
+  end
+
+  def number_of_orders_for(status)
+    orders = []
+
+    merchant_products = self.products 
+    merchant_products.each do |product|
+      product.order_items.each do |item|
+        if item.order.status == status
+          orders << item.order_id
+        end
+      end
+    end
+
+    return orders.uniq.length
   end
 end
