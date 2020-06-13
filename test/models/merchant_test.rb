1d54f347faaf59c7ddd6617da5ccbd1a587a5768
Catherina <catherine.pchelina@gmail.com>
2020-06-13 15:11:02 -0700

    Add model tests for total revenue method

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 describe Merchant do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "custom methods" do 
+    describe "total revenue" do 
+      it "correctly calculates total revenue for one specific merchant" do 
+        merchant = merchants(:merchant_two)
+        product_one = products(:crystal)
+        product_two = products(:wand)
+
+        order = Order.new
+        order.save 
+
+        order_item_one = OrderItem.create(product_id: product_one.id, order_id: order.id, quantity: 2)
+        expect(order_item_one.valid?).must_equal true
+        order_item_two = OrderItem.create(product_id: product_two.id, order_id: order.id, quantity: 2)
+        expect(order_item_two.valid?).must_equal true 
+
+        expect(merchant.total_revenue).must_equal 72
+      end
+
+      it "correctly calculates revenue if order has order_items from different merchants" do 
+        merchant = merchants(:merchant_two)
+        product_one = products(:crystal)
+        product_two = products(:wand)
+        product_two.merchant_id = merchants(:merchant_one).id
+        product_two.save 
+        expect(product_two.merchant_id).must_equal merchants(:merchant_one).id 
+
+        order = Order.new
+        order.save 
+
+        order_item_one = OrderItem.create(product_id: product_one.id, order_id: order.id, quantity: 2)
+        expect(order_item_one.valid?).must_equal true
+        order_item_two = OrderItem.create(product_id: product_two.id, order_id: order.id, quantity: 2)
+        expect(order_item_two.valid?).must_equal true 
+
+        expect(merchant.total_revenue).must_equal 31
+      end
+    end
+  end
 end
