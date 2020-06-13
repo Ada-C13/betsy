978da35f8f6e12b894de6946edfaddb9e5350a7f
Yaz <yasmineoshaugh@gmail.com>
2020-06-13 03:54:40 -0700

    added routes to order_items actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post 'products/:id', to: 'order_items#create', as: 'add_order_item'
+  get '/cart/order_items/:id', to: 'order_items#edit', as: 'order_item'
+  post '/cart/order_items/:id', to: 'order_items#update'
+  delete '/cart/order_items/:id', to: 'order_items#destroy'
+
   get '/cart', to: "orders#cart", as: "cart"
   get '/cart/checkout', to: "orders#checkout", as: "checkout"
   post '/cart/checkout', to: "orders#submit_order"
