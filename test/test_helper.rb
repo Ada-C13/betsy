70aeaf7b6c6e2a7017dba0d4ebe875f2ed559010
Rosalyn <syrosalynyu@gmail.com>
2020-06-12 21:25:16 -0700

    completed tests for merchants controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,4 +18,35 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(merchant)
+    return {
+      provider: merchant.provider,
+      uid: merchant.uid,
+      info: {
+        nickname: merchant.name,
+        email: merchant.email,
+      },
+    }
+  end
+
+  def perform_login(merchant = nil)
+    merchant ||= Merchant.first
+    # telling the omniauth that we need you to create a mock up hash for github, bases on the mock_auth_hash method that we just created
+    OmniAuth.config.mock_auth[:github] = OmniAuth:: AuthHash.new(mock_auth_hash(merchant))
+    
+    # Act try to call the callback route
+    get omniauth_callback_path(:github)
+
+    merchant = Merchant.find_by(uid: merchant.uid, name: merchant.name)
+    expect(merchant).wont_be_nil
+
+    # Verify the user ID was saved - if that didn't work, this test is invalid
+    expect(session[:merchant_id]).must_equal merchant.id
+
+    return merchant
+  end
 end
