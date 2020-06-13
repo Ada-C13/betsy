775f2906052aaf2beadedce77a9a3ed247fd1374
Yaz <yasmineoshaugh@gmail.com>
2020-06-13 01:15:53 -0700

    added routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/cart', to: "orders#cart", as: "cart"
+  get '/cart/checkout', to: "orders#checkout", as: "checkout"
+  post '/cart/checkout', to: "orders#submit_order"
+  get '/orders/:id', to: "orders#show_complete", as: "complete_order"
+  post '/orders/:id', to: "orders#cancel"
+
+
   get 'categories/index'
   get 'categories/new'
   get 'merchants/index'
