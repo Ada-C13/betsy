51f75633416ba6b2595c2c002e0400ce1d063925
Olga Patrakova <olgapatrakova@outlook.com>
2020-06-13 19:17:09 -0700

    A merchant can't create a review for their own product

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
-  validates :text, presence: true, uniqueness: true
+  validates :text, presence: true, uniqueness: { scope: :product_id, message: "not a unique review for this product" }
   validates :rating, presence: true
 end
