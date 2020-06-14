70f03d2191011137bac58b39569482fca04aa7b3
Yaz <yasmineoshaugh@gmail.com>
2020-06-14 16:10:52 -0700

    Fixed bug for changing quantity of order item, have working edit view

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,18 +1,18 @@
 class OrderItemsController < ApplicationController
+  skip_before_action :require_login
   before_action :find_order_item, only: [ :edit, :update, :destroy]
 
   def create
-    qty = params[:order_item][:quantity]
+    qty = params[:quantity].to_i
     product = Product.find_by(id: params[:id])
     order = nil
-    order_item = OrderItem.new(order_item_params)
+    order_item = OrderItem.new(quantity: qty)
     if session[:cart_id]
       order = Order.find_by(id: session[:cart_id])
       existing_order_item = order.find_order_item(product)
       if existing_order_item
-        existing_order_item.quantity += qty
-        flash[:success] = "Added additional #{qty} to cart."
-        redirect_to product_path(product)
+        flash[:redirect] = "#{product.name} already in cart. Edit quantity here:"
+        redirect_to order_item_path(existing_order_item)
         return
       end
       order_item.order = order
@@ -34,6 +34,15 @@ class OrderItemsController < ApplicationController
   def edit; end
 
   def update
+    if @order_item.update(order_item_params)
+      flash[:success] = "Changed quantity."
+      redirect_to product_path(@order_item.product)
+      return
+    else
+      flash.now[:error] = "Couldn't change quantity."
+      render :edit
+      return
+    end
   end
 
   def destroy
