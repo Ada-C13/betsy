fe0d9ab0ee45166053218f1ae65f429383f93e86
Antonia <antonianomamiukor@gmail.com>
2020-06-14 00:34:59 -0700

    created basic validation tests for order items

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe OrderItem do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @order_item = order_items(:order_item_too)
+    describe "relations" do
+      it "belongs to product" do
+        expect(OrderItem.new).must_respond_to :product
+      end
+      it "belongs to order" do
+        expect(OrderItem.new).must_respond_to :order
+      end
+    end
+    
+    describe "validations" do
+      it "must have a quantity"
+      expect(@order_item.valid?).must_equal true
+    end
+    it "must have a positive integer as quantity" do
+      @order_item.quantity = 0
+      expect(@order_item.valid?).must_equal false
+      expect(@order_item.errors.messages).must_include :quantity
+    end
+    it "is invalid without a quantity" do
+      @order_item.quantity =  nil
+      expect(@order_item.valid?).must_equal false
+      expect(@order_item.errors.messages).must_include :quantity
+    end
+  end
 end
