34f97954335a1cff8bbd5720f7f1980064ef5bdd
Rosalyn <syrosalynyu@gmail.com>
2020-06-13 17:34:27 -0700

    created tests for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
   
-  validates :rating, presence: true #, numericality: { only_integer: true }, inclusion: { in: %w(1 2 3 4 5), message: "%{value} is not a valid rating" }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [1,2,3,4,5] , message: "You input is not a valid rating" }
   validates :feedback, presence: true
 end
