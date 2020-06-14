8435a62de1ce4011c58b65d8f5c73274739228c4
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-13 21:55:27 -0700

    adds tests for total and subtotal methods in order and order_items models

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe OrderItem do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "subtotal" do
+    it "calculates the item subtotal accurately" do
+      order_item = order_items(:order_item1)
+      expect(order_item.subtotal).must_equal 8.80
+    end
+  end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 describe Order do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "total" do
+    it "calculates the order total accurately" do
+      order = orders(:pending_order)
+      expect(order.total).must_equal 14.20
+    end
+  end
 end
