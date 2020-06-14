f86dd192d0c758ad2864f17b9e0c4fe053b0ca73
peachmakkoli <lee.higgins.dev@gmail.com>
2020-06-13 22:32:24 -0700

    fixes total and subtotal tests so they account for ruby float precision

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -4,7 +4,7 @@ describe OrderItem do
   describe "subtotal" do
     it "calculates the item subtotal accurately" do
       order_item = order_items(:order_item1)
-      expect(order_item.subtotal).must_equal 8.80
+      expect(order_item.subtotal).must_be_close_to (4.4 + 4.4), 0.001
     end
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,7 +4,7 @@ describe Order do
   describe "total" do
     it "calculates the order total accurately" do
       order = orders(:pending_order)
-      expect(order.total).must_equal 14.20
+      expect(order.total).must_be_close_to (4.4 + 4.4 + 5.4), 0.001
     end
   end
 end
