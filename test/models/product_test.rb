cff2921627bdf61808a7314d7b5e5df871ce6237
Olga Patrakova <olgapatrakova@outlook.com>
2020-06-13 19:03:07 -0700

    Added filters for merchant and products, created tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,12 +2,5 @@ require "test_helper"
 
 # describe Product do
 #   describe "custom methods" do
-#     describe "get_active_products" do 
-#       it "returns only the products where active is set to true" do 
-#         active_products = Product.get_active_products
-
-#         expect(active_products.length).must_equal 1
-#       end
-#     end
 #   end
 # end
