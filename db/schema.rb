554c13574bb4e096c74cc749d6fa0fbb9a94d184
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-14 20:05:45 -0700

    change datatype of cc_cvv

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_06_14_212237) do
-=======
 ActiveRecord::Schema.define(version: 2020_06_15_001432) do
->>>>>>> orders_views
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +54,7 @@ ActiveRecord::Schema.define(version: 2020_06_15_001432) do
     t.string "email"
     t.string "address"
     t.string "cc_last_four"
-    t.integer "cc_cvv"
+    t.string "cc_cvv"
     t.string "status", default: "pending"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
