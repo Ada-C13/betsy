07f3fdaf3ed19736137a2f8a6ac4ac9d24ea07db
Haben <haben7@gmail.com>
2020-06-15 14:41:51 -0700

    added tests for Review controller

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe ReviewsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  let(:product) { products(:daisy) }
+
+  it "should get new" do
+    get new_review_url
+    must_respond_with :success
+  end
+
+  it "should create new review" do
+    assert_difference("Review.count") do
+      post reviews_url, params: { review: { rating: 5, comment: "I was happy with my daisies!", product_id: product.id  } }
+    end
+
+    must_redirect_to review_url(Review.last)
+  end
+
+  it "can't create with out rating" do
+    assert_no_difference("Review.count") do
+      post reviews_url, params: { review: { rating: nil, comment: "I wasn't happy with my daisies!", product_id: product.id  } }
+    end
+
+    must_redirect_to review_path
+  end
+
+  it "can't create when rating that is above 5" do
+    assert_no_difference("Review.count") do
+      post reviews_url, params: { review: { rating: 8, comment: "I was really happy with my daisies!", product_id: product.id  } }
+    end
+
+    must_redirect_to review_path
+  end
 end
