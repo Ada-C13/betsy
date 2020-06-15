fab722888f5c7299598aadf99e0e6447b2cd8f6b
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-15 09:54:36 -0700

    Updated  index action for products to  link with pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
 
   
   def index
-    @products = Product.all
+    @products = Product.paginate(page: params[:page], per_page: 4 )
   end
 
   def show
