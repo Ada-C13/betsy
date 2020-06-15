d4ca293485757b0076e95dce31da6f01ab8c4fd7
Haben <haben7@gmail.com>
2020-06-15 14:38:22 -0700

    added crate and new action to Review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,33 @@
 class ReviewsController < ApplicationController
 
   def new
+    @review = Review.new
   end
 
   def create
+    # if there is a merchant logged in
+    # and that merchant ID matchs the product id
+    # merchant can't review their own product
+
+
+    review = Review.new(review_params)
+
+    if review.save
+      flash[:success] = "Thank you! Your review has been successfully added"
+      redirect_to product_path(review_params[:product_id])
+      return
+    else
+      flash.now[:error] = "Your review hasn't been added."
+      render :new, status: :bad_request
+      return
+    end
+  end
+
+  
+  private
+ 
+  def review_params
+    return params.require(:review).permit(:rating, :comment)
   end
 
 end
