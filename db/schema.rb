79ccece09608759bb651d4f29902eb2b4a22a777
Nikki Vaughan <vnikki13@gmail.com>
2020-06-14 18:36:09 -0700

    Added name column for an order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_12_003901) do
+ActiveRecord::Schema.define(version: 2020_06_15_013058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_06_12_003901) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "customer_email"
+    t.string "name"
   end
 
   create_table "products", force: :cascade do |t|
