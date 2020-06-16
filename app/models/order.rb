ed1552a80b7fde15cbd4e2b8b728f24d5f3c2591
Antonia <antonianomamiukor@gmail.com>
2020-06-16 07:39:12 -0700

    modified order model and its relation and validations tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,13 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
 
-  validates :name, presence: true, on: :update
-  validates :email, presence: true, on: :update
-  validates :cc_number, presence: true, numericality: { only_integer: true }, length: {is: 16 }, on: :update
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :cc_number, presence: true, numericality: { only_integer: true }, length: {is: 16 }
   validates :cc_exp, presence: true
-  validates :mailing_address, presence: true, on: :update
-  validates :zipcode, presence: true, numericality: { only_integer: true}, length: { is: 5 }, on: :update
-  validates :cvv, presence: true, numericality: { only_integer: true }, length: { is: 3 }, on: :update
+  validates :mailing_address, presence: true
+  validates :zipcode, presence: true, numericality: { only_integer: true}, length: { is: 5 }
+  validates :cvv, presence: true, numericality: { only_integer: true }, length: { is: 3 }
 
   def total
     return order_items.map{ |item| item.subtotal}.sum
