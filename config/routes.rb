b8b818a3cf531c8b4d52513676fb80fb981c6f13
Rosalyn <syrosalynyu@gmail.com>
2020-06-17 16:17:32 -0700

    root path is now pointing to the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "products#index"
+  root to: "homepages#index"
   resources :categories
   get '/categories/:id/products', to: 'categories#category_products', as: 'category_product'
 
