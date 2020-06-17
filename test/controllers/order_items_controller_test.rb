9b5002414a6888615a46dc39e29eb206fff0c850
SuelyBarreto <suely@barreto.us>
2020-06-17 09:02:27 -0700

    Improved coverage for OrderItem controller tests

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -31,6 +31,21 @@ describe OrderItemsController do
       must_redirect_to cart_path
     end
 
+    it "Assumes quantity 1 if no quantity" do
+      # Arrange
+      # in the before block
+      # Act & Assert
+      expect {
+        post create_order_items_path(product.id)
+      }.must_change "OrderItem.count", 1
+      new_item = OrderItem.last
+      expect(new_item).wont_be_nil
+      expect(new_item.product.id).must_equal product.id
+      expect(new_item.quantity).must_equal 1
+      must_respond_with :redirect
+      must_redirect_to cart_path
+    end
+
     it "updates quantity if product already in cart" do
       # Arrange
       expect {
@@ -123,6 +138,23 @@ describe OrderItemsController do
       # Act & Assert
       delete order_item_path(item1)
       expect(OrderItem.count).must_equal current_count - 1
+      must_respond_with :redirect
+      must_redirect_to cart_path
+    end
+
+    it "fails to remove an item after order checkout" do
+      # Arrange
+      item1 = OrderItem.new(
+        order_id: order.id,
+        product_id: product.id,
+        quantity: 2
+      )
+      item1.save!
+      current_count = OrderItem.count
+      # Act & Assert
+      item1.order.checkout_order!
+      delete order_item_path(item1)
+      expect(OrderItem.count).must_equal current_count
       must_respond_with :redirect
       must_redirect_to cart_path
     end
