2fb8f8ab6b4458d93d22dab0a8ec93e8b413635d
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-06-18 12:40:05 -0700

    Added titleize to name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -22,12 +22,12 @@ class CategoriesController < ApplicationController
       @category = Category.new(category_params)
       if @category.save
         # flash[:status] = :success
-        flash[:result_text] = "Successfully created #{@category.name}"
+        flash[:result_text] = "Successfully created #{@category.name.titleize}"
         redirect_to account_path(find_user.id)
         return
       else
         flash[:status] = :failure
-        flash[:result_text] = "Could not create a category #{@category.name}"
+        flash[:result_text] = "Could not create a category"
         flash[:messages] = @category.errors.messages
         render :new, status: :bad_request
         return
