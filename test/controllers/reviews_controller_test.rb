9d70c62c9cc1a9fe78c7703c6ed6a71f13a871eb
Haben <haben7@gmail.com>
2020-06-17 19:36:45 -0700

    more review tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -3,10 +3,8 @@ require "test_helper"
 describe ReviewsController do
   let(:product) { products(:daisy) }
 
-
   describe "new" do
     it "can get the review_path" do
-    
       get new_product_review_path(product)
       must_respond_with :success
     end
@@ -17,14 +15,33 @@ describe ReviewsController do
       get new_product_review_path(product)
       expect(flash[:warning]).must_equal "You can't review your own product!"
       must_redirect_to product_path(product)
-      
     end
 
+    #   # hello
+    #   it "should redirect to products_path" do
+    #     bad_review = {
+    #       rating: 8,
+    #       comment: "bad review",
+    #       product_id: -1
+    #     }
+
+    #   post new_product_review_path(-1), params: { review: bad_review }
+    #   #expect(flash[:danger]).must_equal "Something went wrong with the reviewing process."
+    #   must_redirect_to products_path
+    # end
+    it "can't get the review_path for a non-existent product" do
+      product = Product.new(
+        id: 1000,
+        name: "fake",
+      )
+      get new_product_review_path(product)
+      expect(flash[:danger]).must_equal "Something went wrong with the reviewing process."
+    end
   end
 
   it "should create new review" do
     assert_difference("Review.count") do
-      post product_reviews_path(product), params: { review: { rating: 5, comment: "I was happy with my daisies!", product_id: product.id  } }
+      post product_reviews_path(product), params: { review: { rating: 5, comment: "I was happy with my daisies!", product_id: product.id } }
     end
 
     must_redirect_to product_path(product)
@@ -32,7 +49,7 @@ describe ReviewsController do
 
   it "can't create with out rating" do
     assert_no_difference("Review.count") do
-      post  product_reviews_path(product), params: { review: { rating: nil, comment: "I wasn't happy with my daisies!", product_id: product.id  } }
+      post product_reviews_path(product), params: { review: { rating: nil, comment: "I wasn't happy with my daisies!", product_id: product.id } }
     end
 
     must_respond_with :bad_request
@@ -40,22 +57,9 @@ describe ReviewsController do
 
   it "can't create with rating that is above 5" do
     assert_no_difference("Review.count") do
-      post product_reviews_path(product), params: { review: { rating: 8, comment: "I was really happy with my daisies!", product_id: product.id  } }
+      post product_reviews_path(product), params: { review: { rating: 8, comment: "I was really happy with my daisies!", product_id: product.id } }
     end
 
     must_respond_with :bad_request
-  end
-
-# hello 
-  it "should redirect to products_path" do
-    bad_review = {
-      rating: 8,
-      comment: "bad review",
-      product_id: -1
-    }
-
-    post new_product_review_path(-1), params: { review: bad_review }
-    #expect(flash[:danger]).must_equal "Something went wrong with the reviewing process."
-    must_redirect_to products_path
   end
 end
