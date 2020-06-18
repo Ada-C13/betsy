43ffbea6ebd3c2a35342636bcc0ee6cfc394e299
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-06-17 18:45:10 -0700

    delete some old code

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -48,24 +48,4 @@ describe Category do
       expect(category2.errors.messages).must_include :name
     end
   end
-
-  describe "costum_methods" do
-    it "should return all products that belongs to one category" do
-      category1 = Category.new(name: @category1.name)
-
-      products = Category.product_by_category(@category1.name)
-
-      expect(products.length).must_equal 6
-    end
-
-    it "will return empty array if we dont have any product for specific category" do
-      category3 = Category.new(name: @category3.name)
-
-      products = Category.product_by_category(@category3.name)
-      p products
-
-      expect(products.length).must_equal 0
-      expect(products).must_equal []
-    end
-  end
 end
